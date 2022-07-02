81c7892d2ee50e9a6011832bc3f7120aae8a33e0
wildcatsc <octsteven@gmail.com>
2022-07-02 17:44:45 -0400

    .

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -5,6 +5,12 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import pairs from './auth';
 import './Login.css';
 
+function Header() {
+    // Import result is the URL of your image
+    return 
+  }
+
+
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -24,8 +30,8 @@ const Login = () => {
     
     
     return (
-
         <div className="login-window">
+            <img src={require('./girs-banner.png')} id='banner'/>
             <input id = 'username' placeholder="Username..." onChange={(event)=>setUsername(event.target.value)}></input>
             <input id = 'password' placeholder="Password..." onChange={(event)=>setPassword(event.target.value)}></input>
             <Link id = 'login' onClick = {e=>auth()?null:e.preventDefault()} to = {{pathname : `/display?name=${username}`}} state = {{au : cred}}>
